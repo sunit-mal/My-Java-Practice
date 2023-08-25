4e6677b5abf6daa45a4ba4e515fec694f38c602d
sunit-mal <sunitmal1832002@gmail.com>
2023-08-25 22:51:10 +0530

    My Practice Code

--- zzz2.java
+++ zzz2.java
@@ -4,8 +4,6 @@ public class zzz2 {
 
     public static void main(String[] args) {
         System.out.println(java.time.LocalDate.now());
-
-    
-        
+        System.out.println("Hello World");
     }
 }
