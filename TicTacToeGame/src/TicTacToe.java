192e0e824091e6cff8c5e2027f20c518c71b3f93
sunit-mal <sunitmal1832002@gmail.com>
2023-10-31 21:48:44 +0530

    31-10-23

--- TicTacToeGame/src/TicTacToe.java
+++ TicTacToeGame/src/TicTacToe.java
@@ -6,7 +6,7 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
 
   String imageDirectory = System.getProperty("user.dir") + "/imagesForGame/";
 
-  int i, j, ii, jj, x, y, yesnull;
+  int i, j, ii, jj, x, y, checkNull;
   int a[][] = { { 10, 1, 2, 3, 11 }, { 10, 1, 4, 7, 11 }, { 10, 1, 5, 9, 11 }, { 10, 2, 5, 8, 11 },
       { 10, 3, 5, 7, 11 }, { 10, 3, 6, 9, 11 }, { 10, 4, 5, 6, 11 }, { 10, 7, 8, 9, 11 } };
   int a1[][] = { { 10, 1, 2, 3, 11 }, { 10, 1, 4, 7, 11 }, { 10, 1, 5, 9, 11 }, { 10, 2, 5, 8, 11 },
@@ -14,7 +14,7 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
 
   boolean state, type, set;
 
-  Icon ic1, ic2, icon, ic11, ic22;
+  Icon image1, image2, icon, image4, image5;
   Checkbox c1, c2;
   JLabel l1, l2;
   JButton b[] = new JButton[9];
@@ -42,9 +42,8 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
     add(reset);
     reset.addActionListener(this);
 
-  }// eof showButton
+  }
 
-  /*********************************************************/
   public void check(int num1) {
     for (ii = 0; ii <= 7; ii++) {
       for (jj = 1; jj <= 3; jj++) {
@@ -52,16 +51,13 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
           a[ii][4] = 11;
         }
 
-      } // eof for jj
+      }
 
-    } // eof for ii
-  }// eof check
+    } 
+  }
 
-  /**********************************************************/
 
-  /*********************************************************/
-
-  public void complogic(int num) {
+  public void computerLogic(int num) {
 
     for (i = 0; i <= 7; i++) {
       for (j = 1; j <= 3; j++) {
@@ -71,48 +67,48 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
         }
       }
     }
-    for (i = 0; i <= 7; i++) { // for 1
+    for (i = 0; i <= 7; i++) { 
       set = true;
-      if (a[i][4] == 10) { // if 1
+      if (a[i][4] == 10) { 
         int count = 0;
-        for (j = 1; j <= 3; j++) { // for 2
-          if (b[(a[i][j] - 1)].getIcon() != null) { // if 2
+        for (j = 1; j <= 3; j++) { 
+          if (b[(a[i][j] - 1)].getIcon() != null) { 
             count++;
-          } // eof if 2
+          } 
           else {
-            yesnull = a[i][j];
+            checkNull = a[i][j];
           }
-        } // eof for 2
+        } 
         if (count == 2) { // if 2
-          b[yesnull - 1].setIcon(ic2);
-          this.check(yesnull);
+          b[checkNull - 1].setIcon(image2);
+          this.check(checkNull);
           set = false;
           break;
-        } // eof if 2
-      } // eof if 1
+        } 
+      } 
       else if (a[i][0] == 10) {
-        for (j = 1; j <= 3; j++) { // for2
-          if (b[(a[i][j] - 1)].getIcon() == null) { // if 1
-            b[(a[i][j] - 1)].setIcon(ic2);
+        for (j = 1; j <= 3; j++) { 
+          if (b[(a[i][j] - 1)].getIcon() == null) { 
+            b[(a[i][j] - 1)].setIcon(image2);
             this.check(a[i][j]);
             set = false;
             break;
-          } // eof if1
-        } // eof for 2
+          } 
+        } 
         if (set == false)
           break;
-      } // eof elseif
+      } 
 
       if (set == false)
         break;
-    } // eof for 1
+    } 
 
-  }// eof complogic
+  }
 
-  /*********************************************************/
+  
 
   TicTacToe() {
-    super("tic tac toe by aakash");
+    super("tic tac toe");
 
     CheckboxGroup cbg = new CheckboxGroup();
     c1 = new Checkbox("vs computer", cbg, false);
@@ -127,18 +123,18 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
     state = true;
     type = true;
     set = true;
-    ic1 = new ImageIcon(imageDirectory + "ic1.jpg");
-    ic2 = new ImageIcon(imageDirectory + "ic2.jpg");
-    ic11 = new ImageIcon(imageDirectory + "ic11.jpg");
-    ic22 = new ImageIcon(imageDirectory + "ic22.jpg");
+    image1 = new ImageIcon(imageDirectory + "image1.jpg");
+    image2 = new ImageIcon(imageDirectory + "image2.jpg");
+    image4 = new ImageIcon(imageDirectory + "image4.jpg");
+    image5 = new ImageIcon(imageDirectory + "image5.jpg");
 
     setLayout(null);
     setSize(330, 450);
     setVisible(true);
     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-  }// eof constructor
+  }
 
-  /*************************************************************/
+  
   public void itemStateChanged(ItemEvent e) {
     if (c1.getState()) {
       type = false;
@@ -151,13 +147,12 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
     remove(c2);
     repaint(0, 0, 330, 450);
     showButton();
-  }// eof itemstate
-
-  /************************************************************/
+  }
+  
 
   public void actionPerformed(ActionEvent e) {
-    /********************************/
-    if (type == true)// logicfriend
+    
+    if (type == true)
     {
       if (e.getSource() == reset) {
         for (i = 0; i <= 8; i++) {
@@ -169,42 +164,42 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
 
             if (b[i].getIcon() == null) {
               if (state == true) {
-                icon = ic2;
+                icon = image2;
                 state = false;
               } else {
-                icon = ic1;
+                icon = image1;
                 state = true;
               }
               b[i].setIcon(icon);
             }
           }
-        } // eof for
-      } // eof else
-    } // eof logicfriend
-    else if (type == false) { // complogic
+        } 
+      } 
+    } 
+    else if (type == false) { 
       if (e.getSource() == reset) {
         for (i = 0; i <= 8; i++) {
           b[i].setIcon(null);
-        } // eof for
+        } 
         for (i = 0; i <= 7; i++)
           for (j = 0; j <= 4; j++)
-            a[i][j] = a1[i][j]; // again initialsing array
-      } else { // complogic
+            a[i][j] = a1[i][j];
+      } else { 
         for (i = 0; i <= 8; i++) {
           if (e.getSource() == b[i]) {
             if (b[i].getIcon() == null) {
-              b[i].setIcon(ic1);
+              b[i].setIcon(image1);
               if (b[4].getIcon() == null) {
-                b[4].setIcon(ic2);
+                b[4].setIcon(image2);
                 this.check(5);
               } else {
-                this.complogic(i);
+                this.computerLogic(i);
               }
             }
           }
-        } // eof for
+        } 
       }
-    } // eof complogic
+    } 
 
     for (i = 0; i <= 7; i++) {
 
@@ -212,16 +207,16 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
       Icon icon2 = b[(a[i][2] - 1)].getIcon();
       Icon icon3 = b[(a[i][3] - 1)].getIcon();
       if ((icon1 == icon2) && (icon2 == icon3) && (icon1 != null)) {
-        if (icon1 == ic1) {
-          b[(a[i][1] - 1)].setIcon(ic11);
-          b[(a[i][2] - 1)].setIcon(ic11);
-          b[(a[i][3] - 1)].setIcon(ic11);
+        if (icon1 == image1) {
+          b[(a[i][1] - 1)].setIcon(image4);
+          b[(a[i][2] - 1)].setIcon(image4);
+          b[(a[i][3] - 1)].setIcon(image4);
           JOptionPane.showMessageDialog(TicTacToe.this, "!!!YOU won!!! click reset");
           break;
-        } else if (icon1 == ic2) {
-          b[(a[i][1] - 1)].setIcon(ic22);
-          b[(a[i][2] - 1)].setIcon(ic22);
-          b[(a[i][3] - 1)].setIcon(ic22);
+        } else if (icon1 == image2) {
+          b[(a[i][1] - 1)].setIcon(image5);
+          b[(a[i][2] - 1)].setIcon(image5);
+          b[(a[i][3] - 1)].setIcon(image5);
           JOptionPane.showMessageDialog(TicTacToe.this, "!!!AWK (COMPUTER) won!!! click reset");
           break;
         }
@@ -229,8 +224,6 @@ class TicTacToe extends JFrame implements ItemListener, ActionListener {
     }
 
   }
-
-  /************************************************************/
 
   public static void main(String[] args) {
     new TicTacToe();
