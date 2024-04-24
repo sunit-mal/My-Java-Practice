87553590a623a8ec50a5c49388eca593b380c4b0
Sunit Mal <110469858+sunit-mal@users.noreply.github.com>
2024-04-24 19:01:21 +0530

    Delete Collage java code directory

--- Collage java code/ArrayToString.java
+++
@@ -1,9 +0,0 @@
-//  Construct string from subset of char array.
-
-public class ArrayToString {
-    public static void main(String[] args) {
-        char ch[] = {'S','U','N','I','T'};
-        String st = String.copyValueOf(ch,0,3);
-        System.out.println(st);
-    }
-}
--- Collage java code/OneStringToAnotherString.java
+++
@@ -1,9 +0,0 @@
-//  Construct one String from another.
-
-public class OneStringToAnotherString {
-    public static void main(String[] args) {
-        String s1 = "Sunit";
-        String s2 = new String(s1);
-        System.out.println(s2);
-    }
-}
